1754648ff58b5abc6cecfbf87a18d53f1eed5ac9
andrescuxin21 <161546388+andrescuxin21@users.noreply.github.com>
2024-02-27 22:16:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,3 +8,31 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Práctica #2 Bootstrap</title>
+    <!-- Enlace a Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-6 offset-md-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h1 class="card-title">Bienvenido al sitio Web (Andres Cuxin)</h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Enlace a Bootstrap JS (opcional, para funcionalidades avanzadas) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
